6aa830ebb0adb563fc3023527140664c4ac9e1b1
ianphil <ian.philpot@microsoft.com>
2020-02-04 23:30:47 -0500

    Still working on getting tests to run

--- tasks.py
+++ tasks.py
@@ -3,4 +3,21 @@ from invoke import task
 
 @task
 def black(c):
-    c.run('black -l 88 pyRoute13 tests')
+    c.run("black -l 88 pyRoute13 tests")
+
+
+@task
+def test(c):
+    c.run(
+        "nosetests --exe -v --nocapture --with-coverage --cover-erase --cover-package=environment,core,agents,generators"
+    )
+
+
+@task
+def simple(c):
+    c.run('python pyRoute13/simple_simulator.py')
+
+
+@task
+def full(c):
+    c.run('python pyRoute13/full_simulator.py')
